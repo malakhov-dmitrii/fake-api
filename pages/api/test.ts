f1224c45a47478825367ca214e756f99aeadcb16
Malakhov Dmitriy <dmitriy.malakhov@gorparkovki.ru>
2020-07-31 13:37:58 +0300

    added mongodb interation

---
+++ pages/api/test.ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+// Import Dependencies
+const url = require("url");
+const MongoClient = require("mongodb").MongoClient;
+
+// Create cached connection variable
+let cachedDb = null;
+
+// A function for connecting to MongoDB,
+// taking a single parameter of the connection string
+async function connectToDatabase(uri) {
+  // If the database connection is cached,
+  // use it instead of creating a new connection
+  if (cachedDb) {
+    return cachedDb;
+  }
+
+  // If no connection is cached, create a new one
+  const client = await MongoClient.connect(uri, { useNewUrlParser: true });
+
+  // Select the database through the connection,
+  // using the database path of the connection string
+  const db = await client.db(url.parse(uri).pathname.substr(1));
+
+  // Cache the database connection and return the connection
+  cachedDb = db;
+  return db;
+}
+
+// The main, exported, function of the endpoint,
+// dealing with the request and subsequent response
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  // Get a database connection, cached or otherwise,
+  // using the connection string environment variable as the argument
+  console.log(process.env.MONGODB_URI);
+
+  const db = await connectToDatabase(process.env.MONGODB_URI);
+
+  // Select the "users" collection from the database
+  const collection = await db.collection("schemas");
+
+  // Select the users collection from the database
+  const users = await collection.find({ userId: "" }).toArray();
+
+  // Respond with a JSON string of all users in the collection
+  res.status(200).json({ users });
+};
