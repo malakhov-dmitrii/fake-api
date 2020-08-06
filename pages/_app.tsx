370154fe787ce57c3ae56db1c0de3f45f81f0e5e
Malakhov Dmitriy <dmitriy.malakhov@gorparkovki.ru>
2020-08-06 15:07:45 +0300

    version up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,22 @@ const tagManagerArgs = {
   gtmId: "GTM-MWPWFTP",
 };
 
+const YAmetrica = () => `<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(66221665, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/66221665" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->`;
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -39,16 +55,7 @@ export default function MyApp({ Component, pageProps }) {
         />
       </Head>
       <Layout className="layout">
-        <YMInitializer
-          accounts={[66221665]}
-          options={{
-            clickmap: true,
-            trackLinks: true,
-            accurateTrackBounce: true,
-            webvisor: true,
-          }}
-          version="2"
-        />
+        <div dangerouslySetInnerHTML={{ __html: YAmetrica() }}></div>
         <Header>
           <Link href="/">
             <span className="logo mr-15 underline noselect pointer">
